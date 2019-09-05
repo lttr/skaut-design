88bf0c225c54e7e56b230ca1f2e62a3a7c9c4e20
Lukas Trumm <lukas.trumm@centrum.cz>
2019-09-05 09:17:16 +0200

    Add table and tabs examples

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   handleMenuToggle()
+  handleTabButtonsEvents()
 })
 
 function handleMenuToggle() {
@@ -22,4 +23,50 @@ function handleMenuToggle() {
     const menu = document.getElementById(burger.dataset.target)
     menu.classList.toggle('is-active')
   }
+}
+
+function handleTabButtonsEvents() {
+  const tabTrips = document.getElementById('tab-trips')
+  const tabMeetings = document.getElementById('tab-meetings')
+  const tabs = tabTrips.parentElement.children
+  const calendarBody = document.getElementById('calendar-body')
+
+  const trips = [
+    { date: '11. - 12. 4. 2019', name: 'Výprava' },
+    { date: '2. 6. 2019 16:00 - 19:00', name: 'Hra ve městě' },
+    { date: '15. - 17. 6. 2019', name: 'Výprava' },
+    { date: '30. 6. 2019 - 14. 7. 2019', name: 'Tábor' },
+  ]
+
+  const meetings = [
+    { date: '5. 6. 2019', name: 'Schůzka družiny Tygrů' },
+    { date: '12. 6. 2019', name: 'Schůzka družiny Tygrů' },
+    { date: '19. 6. 2019', name: 'Schůzka družiny Tygrů' },
+  ]
+
+  function updateTable(events) {
+    calendarBody.innerHTML = events
+      .map((event) => {
+        return `
+            <tr>
+              <td>${event.date}</td>
+              <td>${event.name}</td>
+            </tr>`
+      })
+      .join('\n')
+  }
+
+  tabMeetings.addEventListener('click', (_) => {
+    updateTable(meetings)
+    Array.from(tabs).forEach((tab) => tab.classList.remove('is-active'))
+    tabMeetings.classList.add('is-active')
+  })
+
+  tabTrips.addEventListener('click', (_) => {
+    updateTable(trips)
+    Array.from(tabs).forEach((tab) => tab.classList.remove('is-active'))
+    tabTrips.classList.add('is-active')
+  })
+
+  updateTable(trips)
 }
